00b65a30eb6efdcbf0965632d4d9d27742c03b8f
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-10-20 16:58:46 +0500

    Task1

---
+++ Tyuiu.VariiMN.Sprint3.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.VariiMN.Sprint3.Task1.V7.Lib;
+
+namespace Tyuiu.VariiMN.Sprint3.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = -0.93;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.VariiMN.Sprint3.Task1.V7/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.VariiMN.Sprint3.Task1.V7.Lib;
+
+
+namespace Tyuiu.VariiMN.Sprint3.Task1.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Варий М. Н. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: цикл for                                                         *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #7                                                             *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич. | ИСПб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет             *");
+            Console.WriteLine("* произведение ряда по формуле, при a=0,25                               *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт = " + startValue);
+            Console.WriteLine("Конец = " + stopValue);
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+        }
+    }
+}
