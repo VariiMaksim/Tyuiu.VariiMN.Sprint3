42b9a920bbf9e75bdd5a1d8626425e592c5f3ef9
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-10-23 22:30:02 +0500

    Task7

---
+++ Tyuiu.VariiMN.Sprint3.Task7.V30.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VariiMN.Sprint3.Task7.V30.Lib
+{
+    public class DataService : ISprint3Task7V30
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((5 * x + 2.5 / Math.Sin(x) + 2) + 2 * x + 2, 2);
+                valueArray[count] = y;
+                count++;
+                if(Math.Sin(x) + 2 == 0)
+                {
+                    y = 0;
+                }
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.VariiMN.Sprint3.Task7.V30/Program.cs
@@ -0,0 +1,67 @@
+﻿using Tyuiu.VariiMN.Sprint3.Task7.V30.Lib;
+
+
+namespace Tyuiu.VariiMN.Sprint3.Task7.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Варий М. Н. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                               *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #30                                                            *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич. | ИСПб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+            Console.WriteLine("Начало отрезка: " + startValue);
+            Console.WriteLine("Конец отрезка: " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("+------+--------+");
+            Console.WriteLine("|  x   |  f(x)  |");
+            Console.WriteLine("+------+--------+");
+            for (int i = 0; i < len; i++)
+            {
+                int x = startValue + i;
+                Console.WriteLine($"| {x,3}  | {valueArray[i],6:F2} |");
+            }
+            Console.WriteLine("+------+--------+");
+            Console.ReadKey();
+
+        }
+    }
+}
