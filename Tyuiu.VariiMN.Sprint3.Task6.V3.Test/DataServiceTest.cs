8cadf27a7a6e63b92c9544a1d2eea95512654e70
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-10-23 21:47:02 +0500

    Task6

---
+++ Tyuiu.VariiMN.Sprint3.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.VariiMN.Sprint3.Task6.V3.Lib;
+
+namespace Tyuiu.VariiMN.Sprint3.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 13;
+            int stopValue = 19;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 121;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VariiMN.Sprint3.Task6.V3/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.VariiMN.Sprint3.Task6.V3.Lib;
+
+
+namespace Tyuiu.VariiMN.Sprint3.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Варий М. Н. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                               *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #3                                                             *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич. | ИСПб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих      *");
+            Console.WriteLine("* числовому отрезку [13, 19] сумму всех делителей больше 8               *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = 13;
+            int stopValue = 19;
+
+
+            Console.WriteLine("Начало отрезка: " + startValue);
+            Console.WriteLine("Конец отрезка: " + stopValue);
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Результат = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
